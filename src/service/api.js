c236d170bb71d0a484e1a87e33038d1c55717813
imtiyaz01 <bhupendra.singh@crownstack.com>
2021-11-01 00:48:33 +0530

    added header

--- src/service/api.js
+++ src/service/api.js
@@ -2,9 +2,15 @@ import axios from 'axios';
 
 const url = 'http://3.140.144.29/api';
 
+const options = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
 export const authenticateLogin = async (login) => {
     try {
-        return await axios.post(`${url}/customer/sendotp`, login)
+        return await axios.post(`${url}/customer/sendotp`, login, options)
     } catch (error) {
         console.log('error while calling login API: ', error);
     }
