f51de0ed874434b92bd6d8b7f676f510357e4a24
imtiyaz01 <bhupendra.singh@crownstack.com>
2021-12-01 18:39:33 +0530

    added mobile to chache

--- src/shared/store/actions/app.actions.js
+++ src/shared/store/actions/app.actions.js
@@ -5,7 +5,7 @@ import flevar from '../../../api/api';
 export const authenticateLogin = payload => dispatch => {
     dispatch({ type: actionTypes.LOGIN, payload });
     return flevar.post(SEND_OTP_API, payload).then(response => {
-        console.log('dtat', response, payload);
+        if (response.status === 200) localStorage.setItem('mobile', payload['mobile']);
     });
 };
 
