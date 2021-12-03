42d35a9bae7980826b080100258e88fb9595d3cf
Bhupendra Singh <30617660+bhupendrasingh4810@users.noreply.github.com>
2021-12-03 15:16:38 +0530

    feature: place order

--- src/pages/Delivering/PaymentButton.jsx
+++ src/pages/Delivering/PaymentButton.jsx
@@ -10,18 +10,18 @@ function PaymentButton() {
 
     const placeOrder = async () => {
         const payload = {
-            "cart_id":Number(cart_id),
-            "address_id":Number(2),
-            "delivery_date":"2021/12/12",
-            "delivery_time_range":"3PM-6PM",
-            "shipment_type":"standard",
-            "shipment_price":"230",
-            "message":"This is message",
-            "special_instruction":"No special Instructions",
-            "coupon_code":"Valentine",
-            "total_discount":100,
-            "payment_vendor":"razorpay",
-            "pincode":700001
+            "cart_id": Number(cart_id),
+            "address_id": Number(2),
+            "delivery_date": "2021/12/12",
+            "delivery_time_range": "3PM-6PM",
+            "shipment_type": "standard",
+            "shipment_price": "230",
+            "message": "This is message",
+            "special_instruction": "No special Instructions",
+            "coupon_code": "Valentine",
+            "total_discount": 100,
+            "payment_vendor": "razorpay",
+            "pincode": 700001
         }
         console.log(payload);
         const order_verification = await flevar.post(VERIFY_ORDER_API, payload);
@@ -40,10 +40,18 @@ function PaymentButton() {
             handler: function (response) {
                 // setPaymentToken(response.razorpay_payment_id);
                 payload['online_payment_id'] = response.razorpay_payment_id;
-                payload['cart_items'] = cartItems;
+                payload['cart_items'] = cartItems[0]['cart_items'].map(item => ({
+                    "cart_item_id": item.id,
+                    "product_id": item.product_id,
+                    "price": item.total_mrp,
+                    "quantity": item.quantity,
+                    "weight": item.cake_weight,
+                    "message": item.message || ""
+                }));
                 console.log(payload);
-                // const order_placed = await flevar.post(PLACE_ORDER_API, payload);
-                // console.log(order_placed);
+                flevar.post(PLACE_ORDER_API, payload).then(response => {
+                    console.log(response);
+                })
                 // console.log(response.razorpay_payment_id);
                 // console.log(response.razorpay_order_id);
                 // console.log(response.razorpay_signature);
